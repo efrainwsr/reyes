0348b57468ca0c136682b65192bf87c98aafbd49
efrainwsr <efrain.figueredowsr@gmail.com>
2023-05-16 00:10:08 -0400

    Armando la vaina

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  
+ import Reyes from './components/Reyes.vue';
+
+</script>
+  
+<template>
+   <div class="container">
+      <div class="row">
+         <div class="col col-sm-4"></div>
+         <div class="col col-sm-4"><Reyes/></div>
+         <div class="col col-sm-4"></div>
+     </div>
+     
+   </div>
+   
+</template>
